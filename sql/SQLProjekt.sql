01c6489a741bff67029418cf4604761b231c8816
Vincent Uber <30577533+vinhero@users.noreply.github.com>
2023-01-14 21:08:19 +0100

    Create sql

---
+++ sql/SQLProjekt.sql
@@ -0,0 +1,99 @@
+-- Datenbank wird erstellt
+CREATE DATABASE SQLProject;
+-- Die Datenbank wird ausgewählt
+USE SQLProject;
+-- Die Tabellen werden erstellt:
+-- Warenkorb
+CREATE TABLE Warenkorb (
+    WbID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Zwischenpreis FLOAT(10, 2) DEFAULT 0.00
+);
+-- Benutzer
+CREATE TABLE Benutzer (
+    BrID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    BenutzerName VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    Passwort VARCHAR(255) NOT NULL,
+    Vorname VARCHAR(255) NOT NULL,
+    Nachname VARCHAR(255) NOT NULL,
+    Geburtsdatum DATE NOT NULL,
+    LetzterLogin DATE NOT NULL,
+    Erstellungsdatum DATE DEFAULT CURRENT_TIMESTAMP,
+    WarenkorbID INT(11),
+    FOREIGN KEY (WarenkorbID) REFERENCES Warenkorb(WbID)
+);
+-- Adressen
+CREATE TABLE Adressen (
+    AeID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    PLZ INT(10) NOT NULL,
+    Ort VARCHAR(255) NOT NULL,
+    StrassenName VARCHAR(255) NOT NULL,
+    HausNr VARCHAR(10) NOT NULL,
+    Land VARCHAR(255) NOT NULL
+);
+-- Produkte
+CREATE TABLE Produkte (
+    PrID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ProduktName VARCHAR(255) NOT NULL,
+    ProduktArt VARCHAR(255) NOT NULL,
+    ProduktPreis FLOAT(10, 2) NOT NULL,
+    ProduktBeschreibung TEXT,
+    AnzahlLagernd INT(11) DEFAULT 0,
+    Verfuegbarkeit BOOLEAN DEFAULT FALSE
+);
+-- Rechnungen
+CREATE TABLE Rechnungen (
+    RnID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Gesamtpreis FLOAT(10, 2) DEFAULT 0.00,
+    RechnungStatus VARCHAR(255) NOT NULL,
+    Rabattcode VARCHAR(255),
+    VersandArt INT(11) NOT NULL,
+    Zahlungsmethode INT(11) NOT NULL,
+    RechnungsadresseID INT(11) NOT NULL,
+    LieferadresseID INT(11) NOT NULL,
+    WarenkorbID INT(11) NOT NULL,
+    FOREIGN KEY (RechnungsadresseID) REFERENCES Adressen(AeID),
+    FOREIGN KEY (LieferadresseID) REFERENCES Adressen(AeID),
+    FOREIGN KEY (WarenkorbID) REFERENCES Warenkorb(WbID)
+);
+CREATE TABLE Rechnungsstatus (
+    StID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    StatusBezeichnung VARCHAR(255) NOT NULL
+);
+CREATE TABLE Rabattcodes (
+    RtID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Code VARCHAR(255) NOT NULL,
+    Wert INT(11) DEFAULT 0
+);
+CREATE TABLE Versandarten (
+    VtID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Lieferant VARCHAR(255) NOT NULL
+);
+CREATE TABLE Zahlungsmethoden (
+    ZeID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Anbieter VARCHAR(255) NOT NULL,
+    Gebuehr FLOAT(10, 2)
+);
+-- Versandländer
+CREATE TABLE Versandlaender (
+    LdID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Landname VARCHAR(255) NOT NULL,
+    Landkuerzel VARCHAR(255) NOT NULL
+);
+-- Verbindungstabelle Benutzer <--> Adressen
+CREATE TABLE Benutzer_Adressen (
+    BenutzerID INT(11) NOT NULL,
+    AdresseID INT(11) NOT NULL,
+    PRIMARY KEY (BenutzerID, AdresseID),
+    FOREIGN KEY (BenutzerID) REFERENCES Benutzer(BrID),
+    FOREIGN KEY (AdresseID) REFERENCES Adressen(AeID)
+);
+-- Verbindungstabelle Warenkorb <--> Produkte
+CREATE TABLE Warenkorb_Produkte (
+    WarenkorbID INT(11) NOT NULL,
+    ProduktID INT(11) NOT NULL,
+    Anzahl INT(2) DEFAULT 1,
+    PRIMARY KEY (WarenkorbID, ProduktID),
+    FOREIGN KEY (WarenkorbID) REFERENCES Warenkorb(WbID),
+    FOREIGN KEY (ProduktID) REFERENCES Produkte(PrID)
+);
